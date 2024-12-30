fa8c2652fc2ae2cbb05eaab2fc4c34f87ef08050
vaibhavapriya <vaibhavapriyandv@gmail.com>
2024-12-30 19:44:22 +0530

    drivecard

--- front-end/pat/src/components/DriveCard.jsx
+++ front-end/pat/src/components/DriveCard.jsx
@@ -13,27 +13,27 @@ function DriveCard({ job }) {
 
     return (
         <li  key={job._id}
-            className="border rounded-lg shadow-lg p-4 bg-white hover:shadow-xl transition-shadow duration-300"
+            className="border rounded-lg shadow-lg p-4 bg-[#F7F4FF] hover:shadow-xl transition-shadow duration-300"
         >
             {isModalOpen && <FormApplication job={job} closeModal={closeModal} />}
             <div className="flex flex-col space-y-2">
                 {/* Drive Title */}
-                <h2 className="text-xl font-semibold text-blue-600">{job.title}</h2>
+                <h2 className="text-xl font-semibold text-[#3D52A0]">{job.title}</h2>
 
                 {/* Package Information */}
-                <div className="text-lg text-gray-700">
+                <div className="text-lg text-[#8697C4]">
                     <span className="font-bold text-black">Package: </span>₹{job.package} LPA
                 </div>
 
                 {/* Description */}
-                <p className="text-sm text-gray-500">{job.description}</p>
-                <button>delailed</button>
+                <p className="text-sm text-[#6B6A85]">{job.description}</p>
+                <button className="text-[#7091E6] hover:underline focus:outline-none">delailed</button>
             </div>
 
-            <div>below this changes if student all drives apply button or applied,if applied drivesin students status as read,shotlisted,interview etc if c/a:edit drive open or close status view applicants button</div>
+            <div classname="mt-4 text-sm text-[#6B6A85]">below this changes if student all drives apply button or applied,if applied drivesin students status as read,shotlisted,interview etc if c/a:edit drive open or close status view applicants button</div>
             {/* Apply Button */}
             <button onClick={handleApply}
-                className="mt-4 w-full py-2 px-4 text-white bg-blue-500 hover:bg-blue-600 rounded-md transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2"
+                className="mt-4 w-full py-2 px-4 text-white bg-[#3D52A0] hover:bg-[#2E4292] rounded-md transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-[#7091E6] focus:ring-offset-2"
             > Apply
             </button>
             
